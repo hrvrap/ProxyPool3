c06914b7fe7f44d2a6da1bb823511417c4fc0cb7
hrvrap <hrvrap>
2019-12-23 16:50:55 +0800

    aaa

---
+++ proxypool/db.py
@@ -0,0 +1,75 @@
+import redis
+from random import choice
+from .error import PoolEmptyError
+from .setting import *
+class RedisClient(object):
+    def __init__(self, host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD):
+        '''decode_responses=True,连接数据库是字符串格式'''
+        self.db = redis.StrictRedis(host=host, port=port, password=password, decode_responses=True)
+
+    def add(self, proxy, score=INITIAL_SCORE):
+        if not  self.db.zscore(REDIS_KEY, proxy):
+            return self.db.zadd(REDIS_KEY, score, proxy)
+
+    def random(self):
+        result = self.db.zrangebyscore(REDIS_KEY, MAX_SCORE, MAX_SCORE)
+        if len(result):
+            return choice(result)
+        else:
+            result = self.db.zrevrange(REDIS_KEY, 0, 100)
+            if len(result):
+                return choice(result)
+            else:
+                raise  PoolEmptyError
+
+    def decrease(self, proxy):
+        score = self.db.zscore(REDIS_KEY, proxy)
+        if score and score > MIN_SCORE:
+            print('代理', proxy, '当前分数', score, '减1')
+            return self.db.zincrby(REDIS_KEY, proxy, -1)
+        else:
+            print('代理', proxy, '当前分数', score, '移除')
+            return self.db.zrem(REDIS_KEY, proxy)
+
+    def exists(self, proxy):
+        return  not self.db.zscore(REDIS_KEY, proxy) == None
+
+    def max(self, proxy):
+        print('代理', proxy, '可用，设置为', MAX_SCORE)
+        return self.db.zadd(REDIS_KEY, MAX_SCORE, proxy)
+
+    def count(self):
+        return self.db.zcard(REDIS_KEY)
+
+    def all(self):
+        return self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
+
+    def batch(self, start, stop):
+        """
+        批量获取
+        :param start: 开始索引
+        :param stop: 结束索引
+        :return: 代理列表
+        """
+        return self.db.zrevrange(REDIS_KEY, start, stop - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
